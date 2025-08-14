dba1c8336003bdbeea856b350c8b525696d5f1d8
kenji005114 <kenji005114@gmail.com>
2025-08-14 22:21:28 +0800

    chore(deps): update dependency react-markdown to v10 (#335)
    * chore(deps): update dependency react-markdown to v10
    
    * chore: the className prop was removed
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Jaguar Zhou <ai.ourship@gmail.com>

--- apps/extension/src/entrypoints/options/routes/Changelog/index.tsx
+++ apps/extension/src/entrypoints/options/routes/Changelog/index.tsx
@@ -3,5 +3,9 @@ import changelog from "@@/CHANGELOG.md?raw";
 import Markdown from "react-markdown";
 
 export function Changelog() {
-  return <Markdown className="prose prose-slate dark:prose-invert">{changelog}</Markdown>;
+  return (
+    <div className="prose prose-slate dark:prose-invert">
+      <Markdown>{changelog}</Markdown>
+    </div>
+  );
 }
